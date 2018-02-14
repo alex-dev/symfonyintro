364a142da72d4b210576c9d5e77c8a9c3728e35b
Alexandre PARENT <1237801@cstj.qc.ca>
2018-02-14 11:07:48 -0500

    changes to db and migration inserts

---
+++ src/AppBundle/Entity/Architecture/ECCTranslations.php
@@ -0,0 +1,45 @@
+<?php
+namespace AppBundle\Entity\Architecture;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Knp\DoctrineBehaviors\Model\Translatable\Translation;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(
+ *   name="ECCTranslations",
+ *   uniqueConstraints={
+ *     @ORM\UniqueConstraint(name="UK_ArchitectureTranslations_name_locale", columns={ "name", "locale" })
+ *   })
+ * @UniqueEntity(fields={ "name", "locale" })
+ */
+class ECCTranslation {
+  use Translation;
+  
+  const name_length = 255;
+
+  /**
+   * @ORM\Column(name="name", type="string", length=ECCTranslation::name_length)
+   */
+  protected $name;
+
+  /**
+   * @return string
+   */
+  public function getName() {
+    return $this->name;
+  }
+
+  /**
+   * @return void
+   * @throws LengthException if $value is longer than self::name_length
+   */
+  public function setName(string $value) {
+    if (mb_strlen($value) > self::name_length) {
+      throw new LengthException("$value must be less then ".self::name_length." characters long.");
+    } else {
+      $this->name = $value;
+    }
+  }
+}
--- src/AppBundle/Entity/Architecture/MemoryArchitecture.php
+++ src/AppBundle/Entity/Architecture/MemoryArchitecture.php
@@ -2,6 +2,7 @@
 namespace AppBundle\Entity\Architecture;
 
 use Doctrine\ORM\Mapping as ORM;
+use AppBundle\Entity\ECC;
 use AppBundle\Entity\Architecture\Architecture;
 
 /**
@@ -9,7 +10,23 @@ use AppBundle\Entity\Architecture\Architecture;
  * @ORM\Table(name="MemoryArchitectures")
  */
 class MemoryArchitecture extends Architecture {
+  /**
+   * @ORM\ManyToOne(
+   *   targetEntity="AppBundle\Entity\ECC",
+   *   cascade={ "persist", "refresh" })
+   * @ORM\JoinColumn(name="idECC", referencedColumnName="idECC", nullable=false)
+   */
+  protected $ECC;
+
   public function __construct(array $names, array $abbreviations) {
     parent::__construct($names, $abbreviations);
+  }
+
+  public function getECC() {
+    return $this->ECC;
+  }
+
+  public function setECC(ECC $value) {
+    $this->ECC = $value;
   }
 }
