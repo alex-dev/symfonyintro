3bb87203c72a435fc2d5ad8867756095c28f0756
Alexandre PARENT <1237801@cstj.qc.ca>
2018-02-15 15:13:58 -0500

    csaf

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -10,8 +10,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class DefaultController extends Controller
 {
   /**
-   * @Route("/", name="list_products", defaults={ "_locale"="%default_locale%" })
-   * @Route("/{_locale}/", name="local_list_products", requirements={ "_locale"="%supported_locales%" })
+   * @Route("/", name="list_products", defaults={ "_locale"="%app.locale%" })
+   * @Route("/{_locale}/", name="local_list_products", requirements={ "_locale"="%app.locales%" })
    * @Method({ "GET" })
    */
   public function indexAction()
