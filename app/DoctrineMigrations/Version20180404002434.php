45240933cc56e3d82568ba3259e460bf60779b88
Alexandre PARENT <1237801@cstj.qc.ca>
2018-04-18 17:41:34 -0400

    Forgot stuff

--- app/DoctrineMigrations/Version20180404002434.php
+++ app/DoctrineMigrations/Version20180404002434.php
@@ -10,7 +10,6 @@ class Version20180404002434 extends AbstractMigration
   public function up(Schema $schema)
   {
     $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
-
     $this->addSql('DROP INDEX UK_Clients_username ON clients');
     $this->addSql('ALTER TABLE clients CHANGE username username VARCHAR(15) NOT NULL, CHANGE username_canonical username_canonical VARCHAR(15) NOT NULL');
   }
@@ -18,7 +17,6 @@ class Version20180404002434 extends AbstractMigration
   public function down(Schema $schema)
   {
     $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
-
     $this->addSql('ALTER TABLE Clients CHANGE username_canonical username_canonical VARCHAR(180) NOT NULL COLLATE utf8_unicode_ci, CHANGE username username VARCHAR(180) NOT NULL COLLATE utf8_unicode_ci');
     $this->addSql('CREATE UNIQUE INDEX UK_Clients_username ON Clients (username)');
   }
