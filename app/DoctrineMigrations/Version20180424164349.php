cd0b233a417eaec4943de5151d0c30efad62cbbf
Alexandre PARENT <1237801@cstj.qc.ca>
2018-04-25 11:19:19 -0400

    Migration fix

--- app/DoctrineMigrations/Version20180424164349.php
+++ app/DoctrineMigrations/Version20180424164349.php
@@ -19,7 +19,6 @@ class Version20180424164349 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE orders ADD stripeToken VARCHAR(255) NOT NULL');
-        $this->addSql('ALTER TABLE orderitems RENAME INDEX idx_7f568f95f5299398 TO IDX_7F568F95C6898E08');
     }
 
     /**
@@ -30,7 +29,6 @@ class Version20180424164349 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE OrderItems RENAME INDEX idx_7f568f95c6898e08 TO IDX_7F568F95F5299398');
         $this->addSql('ALTER TABLE Orders DROP stripeToken');
     }
 }
